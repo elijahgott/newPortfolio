ebe82561f44fa17da8bc27e14ae03af3bbe2e7a0
elijahgott <gottelijah@gmail.com>
2026-09-04 08:59:43 -0500

    added animation to darkModeToggle

--- app/components/darkModeToggle.tsx
+++ app/components/darkModeToggle.tsx
@@ -12,11 +12,13 @@ export default function DarkModeToggle(){
   const {audioUnlocked} = useAudio()
   const {isLight, setIsLight} = useTheme()
 
+  console.log("DarkModeToggle rendered", isLight);
+
   return(
-    <button className="select-none flex items-center p-4 w-full aspect-square rounded-full relative bg-zinc-300/15 backdrop-blur-md shadow-lg overflow-hidden border-2 border-white/40
+    <button className="select-none flex items-center w-full aspect-square rounded-full relative bg-zinc-300/15 backdrop-blur-md shadow-lg overflow-hidden border-2 border-white/40
         hover:scale-105 hover:bg-cyan-400/80 hover:cursor-pointer
         active:scale-95 active:bg-cyan-500/80
-        transition-all duration-75"
+        transition-all duration-100"
         onMouseEnter={() => {
           if(audioUnlocked){
             playSound('hover')
@@ -36,12 +38,9 @@ export default function DarkModeToggle(){
         <div className={`pointer-events-none absolute bottom-4 right-6 bg-radial to-80% h-6 w-6 rounded-xl ${ isLight ? 'from-white/80' : 'from-white/30' }`} />
         <div className={`pointer-events-none absolute bottom-8 right-4 bg-radial to-80% h-4 w-4 rounded-xl ${ isLight ? 'from-white/80' : 'from-white/30' }`} />
 
-        <div className='flex flex-col items-center w-full transition-all duration-500'>
-          {isLight ?
-            <LightModeIcon sx={{fontSize: 80, filter: 'drop-shadow(4px 4px 1px rgba(0, 0, 0, 0.2))'}} className="text-amber-300" />
-            :
-            <DarkModeIcon sx={{fontSize: 80, filter: 'drop-shadow(4px 4px 1px rgba(0, 0, 0, 0.2))'}} className="text-indigo-600" />
-          }
+        <div className='relative flex items-center justify-center w-full aspect-square overflow-hidden rounded-full'>
+          <LightModeIcon sx={{fontSize: 80, filter: 'drop-shadow(4px 4px 1px rgba(0, 0, 0, 0.2))', transition: 'translate 500ms ease-in-out, opacity 500ms ease-in-out',}} className={`text-amber-400 absolute ${isLight ? 'translate-y-0 opacity-100' : 'translate-y-full opacity-0'}`} />
+          <DarkModeIcon sx={{fontSize: 80, filter: 'drop-shadow(4px 4px 1px rgba(0, 0, 0, 0.2))', transition: 'translate 500ms ease-in-out, opacity 500ms ease-in-out',}} className={`text-indigo-600 absolute ${isLight ? 'translate-y-full opacity-0' : 'translate-y-0 opacity-100'}`} />
         </div>
     </button>
   )
