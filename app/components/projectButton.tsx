9e4636c8537cfc3711bf917dea449fc0db2b2c4f
elijahgott <gottelijah@gmail.com>
2026-09-12 18:07:01 -0500

    added disabled state to project buttons to show when a project is not hosted

--- app/components/projectButton.tsx
+++ app/components/projectButton.tsx
@@ -16,18 +16,18 @@ export default function ProjectButton({type, link}: ProjectButtonProps){
   const { audioUnlocked } = useAudio()
   const { isLight } = useTheme()
   return(
-    <div className="select-none flex p-2 w-full relative rounded-full bg-zinc-300/15 backdrop-blur-md shadow-lg overflow-hidden border-2 border-white/40
-          hover:scale-105 hover:bg-cyan-400/80 hover:cursor-pointer
-          active:scale-95 active:bg-cyan-500/80
-          transition-all duration-75"
+    <div className={`select-none flex p-2 w-full relative rounded-full bg-zinc-300/15 backdrop-blur-md shadow-lg overflow-hidden border-2 border-white/40
+          ${ link == 'N/A' ? '' : 'hover:scale-105 hover:bg-cyan-400/80 active:scale-95 active:bg-cyan-500/80'}
+          hover:cursor-pointer
+          transition-all duration-75`}
           onMouseEnter={() => {
-            if(audioUnlocked){
+            if(audioUnlocked && link !== 'N/A'){
               playSound('hover')
             }
           }}
           onClick={(e) => {
             e.stopPropagation()
-            if(audioUnlocked){
+            if(audioUnlocked && link !== 'N/A'){
               playSound('click')
             }
           }}>
@@ -40,8 +40,8 @@ export default function ProjectButton({type, link}: ProjectButtonProps){
           <div className={`z-10 pointer-events-none absolute bottom-8 right-4 bg-radial to-80% h-4 w-4 rounded-xl ${ isLight ? 'from-white/80' : 'from-white/30' }`} />
           {type == 'play' ?
             (
-                <a href={link} target="_blank" className="w-full aspect-square my-auto">
-                  <div className="relative flex items-center justify-center w-full h-full bg-linear-to-tl from-green-500/80 to-lime-400/80 rounded-full">
+                <a href={link} target="_blank" className={`${link == 'N/A' ? 'pointer-events-none' : ''} w-full aspect-square my-auto`}>
+                  <div className={`relative flex items-center justify-center w-full h-full bg-linear-to-tl ${link == 'N/A' ? 'from-zinc-500/80 to-zinc-400/80' : 'from-green-500/80 to-lime-400/80'}  rounded-full`}>
                     <PlayArrowIcon sx={{ fontSize: 80, filter: 'drop-shadow(4px 4px 1px rgba(0, 0, 0, 0.2))' }} className="text-white"/>
                   </div>
                 </a>
