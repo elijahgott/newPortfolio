7284ce3b65165eabf92b97f1d7ce422f2ded8631
elijahgott <gottelijah@gmail.com>
2026-09-19 15:35:07 -0500

    nav psuedo elements are not pill-shaped

--- app/components/nav/navItem.tsx
+++ app/components/nav/navItem.tsx
@@ -27,7 +27,7 @@ export default function NavItem({name, linkTo}: NavItemProps){
         playSound('click')
       }
     }} className={`relative hover:-translate-y-2 hover:scale-105 hover:cursor-pointer active:translate-y-0 active:scale-95 transition-all
-    before:content-[attr(data-name)] before:pointer-events-none before:absolute before:z-50 before:w-max before:bg-linear-to-b ${ isLight ? 'before:from-zinc-300/70 before:to-white before:border-white/75' : 'before:text-white before:from-zinc-600/70 before:to-zinc-900 before:border-zinc-600/75'}  before:border-2  before:shadow-lg before:font-semibold before:p-4 before:rounded-xl before:top-[-50%] before:left-[50%] before:translate-[-50%] before:scale-0 before:opacity-0 before:transition-all before:ease-in
+    before:content-[attr(data-name)] before:pointer-events-none before:absolute before:z-50 before:w-max before:bg-linear-to-b ${ isLight ? 'before:from-zinc-300/70 before:to-white before:border-white/75' : 'before:text-white before:from-zinc-600/70 before:to-zinc-900 before:border-zinc-600/75'}  before:border-2  before:shadow-lg before:font-semibold before:p-4 before:rounded-full before:top-[-50%] before:translate-[-50%] ${ name === 'Elijah Gott | LinkedIn' ? 'before:left-full before:ml-6 md:before:ml-0 md:before:left-[50%]' : 'before:left-[50%]'} before:scale-0 before:opacity-0 before:transition-all before:ease-in
     hover:before:scale-100 hover:before:opacity-100`}
     data-name={name}>{
       <div className="max-w-20 aspect-square">
