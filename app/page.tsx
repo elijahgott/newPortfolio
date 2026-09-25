c6329a941a547f153d2627edaabc625135991d18
elijahgott <gottelijah@gmail.com>
2026-09-25 11:15:03 -0500

    home page now responds to landscape and portrait orientations rather than being screen size based

--- app/page.tsx
+++ app/page.tsx
@@ -58,13 +58,28 @@ export default function Home() {
   return (
     <div className='h-full bg-size-[32px] bg-repeat bg-[url(/images/star-pattern.svg)] background-animation rounded-2xl'>
       <div className={`rounded-2xl h-full flex flex-col flex-1 items-center bg-linear-70 ${isLight ? 'from-zinc-300/90 to-zinc-100' : 'from-zinc-900/90 to-zinc-700'} transition-colors duration-500`}>
-        <div className="flex w-full h-fit my-auto">
-          <div className="absolute top-4 left-4 w-28 md:w-32 aspect-square
-          xl:w-40">
-            <Channel name="Elijah" imageSource='/channelIcons/about-me.jpg' linkTo="about-me" pixelated={false} />
+        <div className="flex flex-col lg:flex-row w-dvw h-fit my-auto">
+          {/* top bar on small screens */}
+          <div className="flex lg:hidden justify-between p-2 w-dvw">
+            <div className="flex w-28 md:w-32 aspect-square
+            xl:w-40">
+              <Channel name="Elijah" imageSource='/channelIcons/about-me.jpg' linkTo="about-me" pixelated={false} />
+            </div>
+            <div className="flex w-28 md:w-32 aspect-square
+            xl:w-40">
+              <DarkModeToggle />
+            </div>
           </div>
-          <motion.main className="w-full sm:w-[80%] lg:aspect-video mx-4 sm:mx-auto mt-8 md:mt-0 grid grid-cols-3 grid-rows-5 gap-4
-          lg:grid-cols-5 lg:grid-rows-3 md:gap-4 md:p-8
+          {/* left bar on med+ screens */}
+          <div className="hidden lg:flex w-fit h-fit px-2">
+            <div className="flex w-28 md:w-32 aspect-square
+            xl:w-40">
+              <Channel name="Elijah" imageSource='/channelIcons/about-me.jpg' linkTo="about-me" pixelated={false} />
+            </div>
+          </div>
+
+          <motion.main className="w-full p-2 sm:w-[80%] lg:aspect-video sm:mx-auto grid portrait:grid-cols-3 portrait:grid-rows-5 gap-4
+          landscape:grid-cols-5 landscape:grid-rows-3 md:gap-4 md:p-8
           lg:gap-8"
           initial={{opacity: 0, y: '-100%'}}
           animate={{opacity: 1, y: 0}}
@@ -73,9 +88,12 @@ export default function Home() {
               return (<Channel key={c.name} name={c.name} imageSource={c.imageSource} linkTo={c.linkTo} pixelated={c.imagePixelated} />)
             })}
           </motion.main>
-          <div className="absolute top-4 right-4 w-28 md:w-32 aspect-square
-          xl:w-40">
-            <DarkModeToggle />
+          {/* right bar on med+ screens */}
+          <div className="hidden lg:flex w-fit h-fit px-2">
+            <div className="flex w-28 md:w-32 aspect-square
+            xl:w-40">
+              <DarkModeToggle />
+            </div>
           </div>
         </div>
       </div>
